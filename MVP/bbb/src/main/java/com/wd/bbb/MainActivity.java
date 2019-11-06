717a448af63995a74c2caff05b1a3df32a61380b
MyJWXGit <2467337415@qq.com>
2019-11-06 21:00:29 +0800

    MVP

--- MVP/bbb/src/main/java/com/wd/bbb/MainActivity.java
+++ MVP/bbb/src/main/java/com/wd/bbb/MainActivity.java
@@ -9,6 +9,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-		//adsadsadsadasdassdadasdas
+		//adsadsadsadasdassdadasdas敖德萨大多所
+		//adsadsadsadasdassdadasdas敖德萨大多所//adsadsadsadasdassdadasdas敖德萨大多所//adsadsadsadasdassdadasdas敖德萨大多所
+		//adsadsadsadasdassdadasdas敖德萨大多所
+		//adsadsadsadasdassdadasdas敖德萨大多所
+		//adsadsadsadasdassdadasdas敖德萨大多所//adsadsadsadasdassdadasdas敖德萨大多所//adsadsadsadasdassdadasdas敖德萨大多所
 	}
 }
